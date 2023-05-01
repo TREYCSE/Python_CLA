241201db54116500f6dcb79b7b8c58eb2bb76362
trey_weathers <treychelle.weathers2017@gmail.com>
2023-04-30 20:09:43 -0400

    Inserted comments for code

--- app.py
+++ app.py
@@ -1,2 +1,21 @@
 import random #choosing random choices
 import os #operating system - for finding files on the computer
+
+#function to grab a random question from a list of questions,
+#then remove tthat question from the list so it doesn't get repeated
+#using global variables to access the questions outside the function
+
+
+#Access a folder on the computer using the folder's path to search for files
+#path = 'C:/Users/treyweathers/Desktop/THE_PLAN/1_EDUCATION/APP_DEV/GENASSEM_FULLSTACK/CODES/Python_CLA'
+#files = ols.listdir(path)
+
+#A list of names eof these file paths are stored as a list in the variable files
+#b/c the only files we want are text files so we are removing files that are not txt
+
+#Shuffle the list of files so that each time we run the program, we will have a different order of flash cards
+#After that, creating a full path for each file
+#opening the file and reading its contents
+
+#CONDITIONALS - y or n
+
