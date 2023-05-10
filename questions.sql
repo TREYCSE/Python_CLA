61fb0c1fce3956969cfb02053b77e383448afef7
trey_weathers <treychelle.weathers2017@gmail.com>
2023-05-10 16:17:42 -0400

    sql and functionality need work

--- questions.sql
+++ questions.sql
@@ -1,16 +1,28 @@
---We're going to start by defining a BaseModel class that sets the database connection:
-class BaseModel(Model):
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+print('no issues!')
+
+class BaseModel(db.Model):
     class Meta:
         database = db
---A Meta class is a class that describes and configures another class. More on Meta classes here
+usr = User()
 
---Now that we have our BaseModel, we can define our model and have it inherit from this BaseModel class:
 class Question(BaseModel):
     question = CharField()
     answer = CharField()
---CharField() and DateField() are from PeeWee's field's.
 
---Once we have the model, we need to add the corresponding table to the database:
 db.create_tables([Question])
 
 artificial = Question(question='What is Artificial Intelligence?', answer='the theory and development of computer systems able to perform tasks that normally require human intelligence, such as visual perception, speech recognition, decision-making, and translation between languages.')
@@ -21,6 +33,4 @@ app.save()
 
 robotics = Question(question="How does robotics work?", answer='To function, a combination of computer programming and algorithms, a remotely controlled manipulator, actuators, control systems -- action, processing and perception -- real-time sensors and an element of automation helps to inform what a robot or robotic system does.')
 robotics.save()
-
---commands: \c database_name then \dt to show all tables, \l lists all databses on the server, DROP DATABASES deletes and CREATE DATABSE creates
 
